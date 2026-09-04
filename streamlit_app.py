e8d5121e08edb3d1848aa3d7390edb9703f03b99
samkihneman-hue <sam.kihneman@gmail.com>
2026-09-04 05:16:16 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
-# Add a selectbox to the sidebar:
-add_button = st.sidebar.button("What", key="what")
-add_button = st.sidebar.button("The", key="the")
-add_button = st.sidebar.button("Fuck", key="fuck")
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
