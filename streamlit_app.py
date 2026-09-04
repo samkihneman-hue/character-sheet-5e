cd9aaa3664a82cb665b824c9013de2429d0b6e49
samkihneman-hue <sam.kihneman@gmail.com>
2026-09-04 05:09:47 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,3 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(dataframe)
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
