e1675cd31bebc51a78657f59ca5b126461c89916
samkihneman-hue <sam.kihneman@gmail.com>
2026-09-04 04:52:41 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 import pandas as pd
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'first column': ["STR", "DEX", "CON", "INT", "WIS", "CHA"],
+  'second column': ["+4", "+2", "+1", "+0", "-1", "-2"]
 })
 
 df
