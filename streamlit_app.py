3c81283e7a484a43f23f37465a755fb1f943dba7
samkihneman-hue <sam.kihneman@gmail.com>
2026-09-04 04:50:36 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("Sammy")
-st.write(
-    "It's Working!"
-)
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
